340a3ff5a66b0c1f2476d1feeac9babf4c437478
Tomás Altrui <tomas.altrui@widergy.com>
2022-03-15 21:10:05 -0300

    Initial commit

---
+++ src/main/java/com/adoo/grupo7/api/gestionconsorcio/GestionconsorcioApplication.java
@@ -0,0 +1,13 @@
+package com.adoo.grupo7.api.gestionconsorcio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionconsorcioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionconsorcioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adoo/grupo7/api/gestionconsorcio/GestionconsorcioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adoo.grupo7.api.gestionconsorcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionconsorcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
